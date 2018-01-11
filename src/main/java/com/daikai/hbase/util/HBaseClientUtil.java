a2f086e5ef53dbe24b8c809aaca70ec3a08630c0
daikai0902 <1040526401@qq.com>
2018-01-11 14:12:21 +0800

    适配1.x版本hbase

--- src/main/java/com/daikai/hbase/util/HBaseClientUtil.java
+++ src/main/java/com/daikai/hbase/util/HBaseClientUtil.java
@@ -3,10 +3,15 @@ package com.daikai.hbase.util;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,7 +20,11 @@ import java.util.Map;
  */
 public class HBaseClientUtil {
 
+    private static Logger logger  = Logger.getLogger(HBaseClientUtil.class);
+
     private  static  Configuration configuration;
+
+
 
     static {
         configuration = HBaseConfiguration.create();
@@ -24,84 +33,128 @@ public class HBaseClientUtil {
     }
 
 
+
+
+    /**
+     * 新建表(包含删除功能)
+     * @Date: 13:59 2018/1/10
+     */
+    public static void createTable(String tableName,String[] columnStrs){
+        Connection connection = null;
+        Admin admin = null;
+        try {
+            connection =  ConnectionFactory.createConnection(configuration);
+            admin = connection.getAdmin();
+            if(admin.tableExists(TableName.valueOf(tableName))){
+                admin.disableTable(TableName.valueOf(tableName));
+                admin.deleteTable(TableName.valueOf(tableName));
+                logger.info("表已经存在，先删除后创建");
+            }
+            HTableDescriptor htable = new HTableDescriptor(TableName.valueOf(tableName));
+            for(String columnStr:columnStrs){
+                htable.addFamily(new HColumnDescriptor(columnStr));
+            }
+            admin.createTable(htable);
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }finally {
+            try {
+                if (admin != null) {
+                    admin.close();
+                }
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+
+
+
+    /**
+     * 插入数据
+     * @Date: 16:02 2018/1/10
+     */
+    public static void insertData(String tableName){
+        Connection connection = null;
+        try {
+            connection =  ConnectionFactory.createConnection(configuration);
+            Table table  = connection.getTable(TableName.valueOf(tableName));
+            Put put = new Put("00004".getBytes());
+            put.addColumn("personInfo".getBytes(),"name".getBytes(),"daikai".getBytes());
+            put.addColumn("personInfo".getBytes(),"age".getBytes(),"23".getBytes());
+            put.addColumn("personInfo".getBytes(),"sex".getBytes(),"male".getBytes());
+            table.put(put);
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+
+
+    /**
+     * 根据rowkey删除某行
+     * @Date: 16:08 2018/1/10
+     */
+    public static void deleteRow(String tableName,String rowkey){
+        Connection connection = null;
+        try {
+            connection =  ConnectionFactory.createConnection(configuration);
+            Table table  = connection.getTable(TableName.valueOf(tableName));
+            Delete delete = new Delete(rowkey.getBytes());
+            table.delete(delete);
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+
     /**
      * 查询
-     * @Date: 10:10 2018/1/10
+     * @Date: 13:57 2018/1/11
      */
-    public static Result getResult(String tableName, String rowkey) {
+    public static void queryAll(String tableName){
         try {
-            HTable table = new HTable(configuration, tableName);
-            Get get = new Get(rowkey.getBytes());
-            return table.get(get);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+            Connection connection = ConnectionFactory.createConnection(configuration);
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            Scan scan = new Scan();
+            scan.setCaching(200);
+            scan.setCacheBlocks(false);
+            ResultScanner scanner = table.getScanner(scan);
+            for (Result result:scanner){
+                String s =   Bytes.toString(result.getValue(Bytes.toBytes("personInfo"),Bytes.toBytes("name"))) ;
+                System.err.println(s);
 
-
-    // 创建表
-    public static boolean create(String tableName, String columnFamily) {
-        HBaseAdmin admin = null;
-        try {
-            admin = new HBaseAdmin(configuration);
-            if (admin.tableExists(tableName)) {
-                System.out.println(tableName + " exists!");
-                return false;
-            } else {
-                // 逗号分隔，可以有多个columnFamily
-                String[] cfArr = columnFamily.split(",");
-                HColumnDescriptor[] hcDes = new HColumnDescriptor[cfArr.length];
-                for (int i = 0; i < cfArr.length; i++) {
-                    hcDes[i] = new HColumnDescriptor(cfArr[i]);
-                }
-                HTableDescriptor tblDes = new HTableDescriptor(TableName.valueOf(tableName));
-                for (HColumnDescriptor hc : hcDes) {
-                    tblDes.addFamily(hc);
-                }
-                admin.createTable(tblDes);
-                System.out.println(tableName + " create successfully！");
-                return true;
             }
-        } catch (IOException e) {
+        }catch (IOException e){
             e.printStackTrace();
-            return false;
         }
     }
 
 
-    //插入数据
-    public static boolean put(String tableName, String rowkey, String columnFamily, String qualifier, String value) {
+
+    /**
+     * 指定查询条件
+     * @Date: 14:06 2018/1/11
+     */
+    public static void queryByCondition(String tableName){
         try {
-            HTable table = new HTable(configuration, tableName);
-            Put put = new Put(rowkey.getBytes());
-            put.add(columnFamily.getBytes(), qualifier.getBytes(), value.getBytes());
-            table.put(put);
-            System.out.println("put successfully！ " + rowkey + "," + columnFamily + "," + qualifier + "," + value);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+            Connection connection = ConnectionFactory.createConnection(configuration);
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            Scan scan = new Scan();
+            Filter filter  = new SingleColumnValueFilter(Bytes.toBytes("personInfo"),Bytes.toBytes("name"), CompareFilter.CompareOp.EQUAL,Bytes.toBytes("qianjiang"));
+            scan.setFilter(filter);
+            ResultScanner scanner = table.getScanner(scan);
+            for (Result result:scanner){
+                String s =   Bytes.toString(result.getValue(Bytes.toBytes("personInfo"),Bytes.toBytes("name"))) ;
+                System.err.println(s);
 
-
-
-    //指定qualifier查询数据
-    public static byte[] get(String tableName, String rowkey, String qualifier) {
-        System.out.println("get result. table=" + tableName + " rowkey=" + rowkey + " qualifier=" + qualifier);
-        Result result = getResult(tableName, rowkey);
-        if (result != null && result.listCells() != null) {
-            for (Cell cell : result.listCells()) {
-                String key = Bytes.toString(CellUtil.cloneQualifier(cell));
-                if (key.equals(qualifier)) {
-                    String value = Bytes.toString(CellUtil.cloneValue(cell));
-                    System.out.println(key + " => " + value);
-                    return CellUtil.cloneValue(cell);
-                }
             }
+        }catch (IOException e){
+            e.printStackTrace();
         }
-        return null;
     }
 
 
@@ -124,8 +177,14 @@ public class HBaseClientUtil {
 
 
     public static void main(String[] args) {
-        Result result =  HBaseClientUtil.getResult("user","10001");
-        HBaseClientUtil.result2Map(result);
+        Long time1 = System.currentTimeMillis();
+       // String[] strs = {"personInfo"};
+       // HBaseClientUtil.createTable("daikai_test",strs);
+       //HBaseClientUtil.deleteRow("daikai_test","00001");
+        HBaseClientUtil.queryAll("daikai_test");
+       // HBaseClientUtil.insertData("daikai_test");
+        Long time2 = System.currentTimeMillis();
+        logger.info("==========执行结束,耗时:"+(time2-time1)/1000+"s============");
     }
 
 
